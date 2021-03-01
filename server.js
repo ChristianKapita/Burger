6029517606f0e3c744192371fe0fd2219dee3cc9
chrisZip <kapitachristian2003@gmail.com>
2021-02-28 23:38:48 -0500

    added routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express= require("express");
 const PORT=process.env.PORT|| 8080;
-const app=express;
+const app=express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -15,3 +15,13 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// Import routes and give the server access to them.
+const routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
