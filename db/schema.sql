4e22a29db76f9bee24a79abff3ad7025963921a2
chrisZip <kapitachristian2003@gmail.com>
2021-02-28 21:21:49 -0500

    App directories structure

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id int not null auto_increment,
+    burger_name varchar(50),
+    devoured boolean,
+    primary key(id)
+)
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Insert data in burgers table
+USE burgers_db;
+INSERT INTO burgers SET burger_name="Big Mac", devoured = false;
+INSERT INTO burgers SET burger_name="Double Cheeseburger", devoured = false;
+INSERT INTO burgers SET burger_name="Bacon BBQ Burger", devoured = false;
+
+
